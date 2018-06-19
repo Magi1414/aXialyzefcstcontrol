3e9f3d85d42194d7bf398feec9c83e8ca71f95bd
Koen Slootmans <koen.slootmans@axialyze.com>
2018-06-19 17:45:01 +0200

    update before test

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,27 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+require("RPostgreSQL")
+require(forecast)
+require("tsoutliers")
+require(ggplot2)
+require("tsintermittent")
+
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, dbname = "Atotech",
+                 host = "axialyzeproduction.c5drkcatbgmm.eu-central-1.rds.amazonaws.com", port = 5432,
+                 user = "aXialyze", password = "aXialyze0000")
+df <- dbGetQuery(con, "SELECT materialghost, kilo, periods, order_periods, continuity, time_serie_category,
+                          order_value FROM public.time_serie_categories_ghosts where time_serie_category = 'Continuous'" )
+
+ f <- function(x, connection) {
+ phantom <- x[1]
+ fcaccuracy <- fcstPhantom(connection , Phantom, FALSE, 'YYYY-IW', 52 )
+
+ qry = "insert into fcst_accuracy (fcst_accuracy_measurement, material, geography, MAPE, created_date,
+            output_description) values ($1,$2,$3,$4,$5, $6)"
+ dbSendQuery(connection, qry, c("phantom_overall_Continous", phantom, "Global", fcaccuracy$MAPE, now(), fcaccuracy))
+}
+
+ ##apply(df, 1, f, connection = con)
--- R/main_functions.R
+++ R/main_functions.R
@@ -12,14 +12,57 @@ fcstPhantom <- function( connection , Phantom, intermittent, DateMask, yrfreq) {
 
 }
 
+Clean_Phantom_Cluster <- function( connection , Phantom, Cluster, DateMask, yrfreq) {
+
+  query1 <- paste("select requested_deliv_date,liters from get_orderqty_per_date_inclmaterialghost_cluster(", Phantom, "")
+  query2 <- paste(query1, ",", "")
+  query2 <- paste(query2, Cluster, "")
+  query2 <- paste(query2, ",", "")
+  query3 <- paste(query2, DateMask, "")
+  query <- paste(query3 , ")","")
+  df_postgres <- RPostgreSQL::dbGetQuery(connection,  query)
+
+  myts <- ts(df_postgres[ ,2], start = c(2015, 1), frequency = yrfreq)
+  c <- tsoutliers::tso( y = myts, types = c("AO",  "TC", "SLS"),
+                        maxit = 1, discard.method = "en-masse", tsmethod = "auto.arima",
+                        args.tsmethod = list(allowdrift = TRUE, ic = "bic"))
+  c$myts <- myts
+  return (c)
+
+}
+
+Clean_Phantom_Cluster_Customer <- function( connection , Phantom, Cluster, Customer, DateMask, yrfreq) {
+
+  query1 <- paste("select requested_deliv_date,liters from get_orderqty_per_date_inclmaterialghost_cluster_division_custom(", Phantom, "")
+  query2 <- paste(query1, ",", "")
+  query2 <- paste(query2, Cluster, "")
+  query2 <- paste(query2, ",", "")
+  query2 <- paste(query2, DateMask, "")
+  query2 <- paste(query2, ",", "")
+  query3 <- paste(query2, Customer, "")
+  query <- paste(query3 , ")","")
+  print(query)
+
+  df_postgres <- RPostgreSQL::dbGetQuery(connection,  query)
+
+  myts <- ts(df_postgres[ ,2], start = c(2015, 1), frequency = yrfreq)
+  print(myts)
+  c <- tsoutliers::tso( y = myts, types = c("AO",  "TC", "SLS"),
+                        maxit = 1, discard.method = "en-masse", tsmethod = "auto.arima",
+                        args.tsmethod = list(allowdrift = TRUE, ic = "bic"))
+  c$myts <- myts
+  return (c)
+
+}
+
 fcstgetAccuracy <- function(myts, intermittent){
   if(intermittent == TRUE){
     fcst <- tsintermittent::imapa(myts)
   }
   else{
     c <- tsoutliers::tso( y = myts, types = c("AO",  "TC", "SLS"),
-              maxit = 1, discard.method = "en-masse", tsmethod = "auto.arima",
-              args.tsmethod = list(allowdrift = TRUE, ic = "bic"))
+                          maxit = 1, discard.method = "en-masse", tsmethod = "auto.arima",
+                          args.tsmethod = list(allowdrift = TRUE, ic = "bic"))
     fcst <- forecast::auto.arima(c$yadj)
   }
   result <- forecast::accuracy(fcst)
