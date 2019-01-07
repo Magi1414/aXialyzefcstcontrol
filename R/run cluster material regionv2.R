ecd78df724c5ead033a7b0949f354286bb6d9f1e
Koen Slootmans <koen.slootmans@axialyze.com>
2019-01-07 10:47:09 +0100

    dcd

--- R/run cluster material regionv2.R
+++ R/run cluster material regionv2.R
@@ -14,7 +14,7 @@ con <- dbConnect(drv, dbname = "Atotech",
 df <- dbGetQuery(con, "SELECT material, cluster,  sales_organization , totalvolume,  ts_categorie, case when sma_only then 1 else 0 end
                  FROM public.sandop_selection_region s
                  where
-                 not exists  (select material || geography from fcst_accuracy f where fcrun = '20181104-2'
+                 not exists  (select material || geography from fcst_accuracy f where fcrun = (select fcrun_gmf from current_run)
                  and f.material = s.material and left(geography,4) = sales_organization )
                  AND prod_group ilike '1%' and not prod_group = '106' and cluster = 'China' and totalvolume > 0
                  order by totalvolume desc" )
@@ -27,28 +27,33 @@ df <- dbGetQuery(con, "SELECT material, cluster,  sales_organization , totalvolu
 no_cores <- detectCores() - 1
 
 run_mat_cust_mm <- function(df, no_cores) {
-    foreach(i=1:no_cores) %dopar%
-    {
-      require("RPostgreSQL")
-      require(forecast)
-      require("tsoutliers")
-      require(ggplot2)
-      require("tsintermittent")
-      source('~/aXialyzefcstcontrol/R/main_functions.R')
-      source('~/aXialyzefcstcontrol/R/material_regionv2.R')
-      drv <- dbDriver("PostgreSQL")
-      con <- dbConnect(drv, dbname = "Atotech",
-                       host = "axialyzeproduction.c5drkcatbgmm.eu-central-1.rds.amazonaws.com", port = 8080,
-                       user = "aXialyze", password = "aXialyze0000")
-      batchsize <- floor(nrow(df)/no_cores)
-      startnr <- batchsize*i - batchsize + 1
-      if(batchsize*(i+1) > nrow(df)){endnr <- nrow(df) }else {endnr <-  batchsize*i}
-      dfall <- df[startnr:endnr, ]
-      level <- "material_region_Continous"
-      fcrun <- "20181104-2"
-      todate <- "2018-10-31"  #parameter for last date of history to take into account
-     apply(dfall, 1, f_mat_regi, connection = con, ilevel = level,  iYYYY = "YYYY-MM"  ,ifreq = 12, "10.2018", TRUE, fcrun, todate)
-    }}
+  foreach(i=1:no_cores) %dopar%
+  {
+    require("RPostgreSQL")
+    require(forecast)
+    require("tsoutliers")
+    require(ggplot2)
+    require("tsintermittent")
+    source('~/R/aXialyzefcstcontrol/R/main_functions.R')
+    source('~/R/aXialyzefcstcontrol/R/material_regionv2.R')
+    drv <- dbDriver("PostgreSQL")
+    con <- dbConnect(drv, dbname = "Atotech",
+                     host = "axialyzeproduction.c5drkcatbgmm.eu-central-1.rds.amazonaws.com", port = 8080,
+                     user = "aXialyze", password = "aXialyze0000")
+
+    df2 <-  dbGetQuery(con, "SELECT fcrun, to_char(requested_deliv_date_to, 'yyyy-mm-dd') as requested_deliv_date_to , fcperiod FROM public.parameter_sets ps where ps.fcrun = (SeLECT fcrun_gmf FROM current_run)" )
+
+
+    batchsize <- floor(nrow(df)/no_cores)
+    startnr <- batchsize*i - batchsize + 1
+    if(batchsize*(i+1) > nrow(df)){endnr <- nrow(df) }else {endnr <-  batchsize*i}
+    dfall <- df[startnr:endnr, ]
+    level <- "material_region_Continous"
+    fcrun <- df2[,"fcrun"]
+    todate <- df2[,"requested_deliv_date_to"]  #parameter for last date of history to take into account
+    fcperiod <- df2[,"fcperiod"]
+    apply(dfall, 1, f_mat_regi, connection = con, ilevel = level,  iYYYY = "YYYY-MM"  ,ifreq = 12, fcperiod, TRUE, fcrun, todate)
+  }}
 # Initiate cluster
 cl <- makeCluster(no_cores)
 doParallel::registerDoParallel(cl)
